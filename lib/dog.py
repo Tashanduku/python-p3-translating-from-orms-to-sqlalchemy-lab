828fe05f211185d19198a19f73a867992cddd542
Natasha Nduku <natashanduku0@gmail.com>
2025-03-19 22:30:11 +0300

    lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,36 @@
-from models import Dog
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from lib.models import Dog, Base
 
-def create_table(base):
-    pass
+SQLITE_URL = "sqlite:///dogs.db"
+
+def create_table(base, engine):
+    """Creates a SQLite database using the given declarative_base."""
+    base.metadata.create_all(engine)
 
 def save(session, dog):
-    pass
+    """Saves a Dog instance to the database and ensures it's the only entry."""
+    session.query(Dog).delete() 
+    session.add(dog)
+    session.commit()
 
 def get_all(session):
-    pass
+    """Returns all Dog instances from the database."""
+    return session.query(Dog).all()
 
 def find_by_name(session, name):
-    pass
+    """Finds a dog by name."""
+    return session.query(Dog).filter_by(name=name).first()
 
-def find_by_id(session, id):
-    pass
+def find_by_id(session, dog_id):
+    """Finds a dog by ID."""
+    return session.query(Dog).filter_by(id=dog_id).first()
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    """Finds a dog by name and breed."""
+    return session.query(Dog).filter_by(name=name, breed=breed).first()
 
-def update_breed(session, dog, breed):
-    pass
+def update_breed(session, dog, new_breed):
+    """Updates the breed of the given dog."""
+    dog.breed = new_breed
+    session.commit()
